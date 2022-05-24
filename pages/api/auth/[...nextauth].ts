7837061057c3edd75c423e7c2b6e5be3c724ea70
Nate Look <nate@natelook.com>
2022-05-24 11:57:19 -0700

    clean up

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,24 +8,6 @@ export default NextAuth({
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID!,
       clientSecret: process.env.DISCORD_SECRET!,
-
-      // profile(profile: DiscordProfile) {
-      //   if (profile.avatar === null) {
-      //     const defaultAvatarNumber = parseInt(profile.discriminator) % 5;
-      //     profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`;
-      //   } else {
-      //     const format = profile.avatar.startsWith('a_') ? 'gif' : 'png';
-      //     profile.image_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`;
-      //   }
-      //   return {
-      //     id: profile.id,
-      //     verified: profile.verified,
-      //     discord: `${profile.username}#${profile.discriminator}`,
-      //     pfp: profile.image_url,
-      //     name: profile.username,
-      //     email: profile.email,
-      //   };
-      // },
     }),
   ],
   callbacks: {
