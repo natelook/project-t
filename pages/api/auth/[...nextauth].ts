59ec960c287912aee718e708255f417cc17bb34b
Nate Look <nate@natelook.com>
2022-06-16 23:41:45 -0700

    add email login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,14 @@
 import NextAuth from 'next-auth/next';
-import DiscordProvider from 'next-auth/providers/discord';
+import EmailProvider from 'next-auth/providers/email';
 import prisma from '@lib/prisma';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 // import makeNoun from '@lib/nouns-playground';
 
 export default NextAuth({
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID!,
-      clientSecret: process.env.DISCORD_SECRET!,
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
     }),
   ],
   callbacks: {
